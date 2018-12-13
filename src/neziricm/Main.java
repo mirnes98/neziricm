c18b86d98c724b4f50aab984203ef05c1223c13a
Mirnes Neziric <neziricm@msoe.edu>
2018-12-13 10:24:14 -0600

    Added bee image
    Signed-off-by: Mirnes Neziric <neziricm@msoe.edu>

--- src/neziricm/Main.java
+++ src/neziricm/Main.java
@@ -1,23 +1,37 @@
 package neziricm;
 
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
 import javafx.application.Application;
-import javafx.fxml.FXMLLoader;
-import javafx.scene.Parent;
+import javafx.scene.Group;
 import javafx.scene.Scene;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
 import javafx.stage.Stage;
 
 public class Main extends Application {
-
     @Override
-    public void start(Stage primaryStage) throws Exception{
-        Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
-        primaryStage.setTitle("Hello World");
-        primaryStage.setScene(new Scene(root, 300, 275));
-        primaryStage.show();
+    public void start(Stage stage) throws FileNotFoundException {
+        Image image = new Image(new FileInputStream("src/tenor.gif"));
+        ImageView imageView = new ImageView(image);
+
+        imageView.setX(0);
+        imageView.setY(0);
+        imageView.setFitHeight(600);
+        imageView.setFitWidth(600);
+        imageView.setPreserveRatio(true);
+
+        Group root = new Group(imageView);
+
+        Scene scene = new Scene(root, 600, 500);
+
+        stage.setTitle("Bees!");
+
+        stage.setScene(scene);
+
+        stage.show();
     }
-
-
-    public static void main(String[] args) {
+    public static void main(String args[]) {
         launch(args);
     }
 }
