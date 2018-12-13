c98714c88ead5046ac78a54a271d704217bb379f
Mirnes Neziric <neziricm@msoe.edu>
2018-12-13 10:27:15 -0600

    Added credits
    Signed-off-by: Mirnes Neziric <neziricm@msoe.edu>

--- src/neziricm/Controller.java
+++ src/neziricm/Controller.java
@@ -1,3 +1,11 @@
+/*
+ * Course:     SE 2811
+ * Term:       Winter 2018-19
+ * Assignment: Lab 2
+ * Author:     Mirnes Neziric
+ * Date:       12/13/18
+ */
+
 package neziricm;
 
 public class Controller {
