53d4538860539b06ab6642d89f726e2177f8c6db
Mirnes Neziric <neziricm@msoe.edu>
2018-12-13 09:53:15 -0600

    I hope git works
    Signed-off-by: Mirnes Neziric <neziricm@msoe.edu>

--- src/neziricm/Controller.java
+++ src/neziricm/Controller.java
@@ -1,4 +1,4 @@
-package sample;
+package neziricm;
 
 public class Controller {
 }
--- src/neziricm/Main.java
+++ src/neziricm/Main.java
@@ -1,4 +1,4 @@
-package sample;
+package neziricm;
 
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
